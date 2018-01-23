f9de5a47952c6362ba186615100a485a9bfb0451
paulohome <bd.paulovictor@gmail.com>
2018-01-23 17:06:21 -0300

    State e Ciclo de Vida

--- app/src/HelloWorld.js
+++ app/src/HelloWorld.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
-
+import StateInput from './StateInput'
 
 class HelloWorld extends Component {
     render() {
+        const result =4
         return (
             <div className="my-class-from-react">
+                <StateInput/>
                 <h1>Hello World From React.js</h1>
+                <h4>{result === 4 ? 'TRUE':'FALSE'}</h4>
             </div>
         )
     }
